51a75b6ecd99b89b3189106e60b195e7eb7e36be
Ritik kumar <75531808+Ritikkumar992@users.noreply.github.com>
2024-02-14 10:56:28 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/apiapp/MainActivity.java
+++ app/src/main/java/com/example/apiapp/MainActivity.java
@@ -12,9 +12,8 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-// MainActivity Class.
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
     // variable declaration
     private RecyclerView recyclerView;
     private CountryAdapter adapter;
@@ -25,31 +24,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // recycler view
+        // recycler view called
         recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new CountryAdapter();
         recyclerView.setAdapter(adapter);
-
-        loadCountries(); // load country data from API.
+        
+        // loadCountry() method called.
+        loadCountries(); 
     }
-    private void loadCountries() { // fetching country data.
+    // loadCountry() method declared.
+    private void loadCountries() {
         CountryApi countryApi = RetrofitClient.getClient().create(CountryApi.class);
         // API Call
         Call<List<Country>> call = countryApi.getCountries("name,capital,flag");
         call.enqueue(new Callback<List<Country>>() {
             @Override
             public void onResponse(Call<List<Country>> call, Response<List<Country>> response) {
-                // success
+                // successfull response
                 if (response.isSuccessful() && response.body() != null) {
                     adapter.setCountries(response.body());
                 }
-                // fail
+                // failed response.
                 else {
                     Toast.makeText(MainActivity.this, "Failed to fetch data", Toast.LENGTH_SHORT).show();
                 }
             }
-            // handling API failure.
+            // If API is failed => handle the below lines.
             @Override
             public void onFailure(Call<List<Country>> call, Throwable t) {
                 Log.e("API Error", "Error fetching data", t);
